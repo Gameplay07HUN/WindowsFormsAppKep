825d9c051cfde09d4ed203d1b8f13fca78480193
Gameplay07HUN <gameplay719@gmail.com>
2022-12-14 09:11:17 +0100

    Add project files.

---
+++ WindowsFormsAppKep/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsAppKep
+{
+    public partial class Form1 : Form
+    {
+        List<Image> Images;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonKep_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "*.jpg|*.jpg|*.png|*.png|*.webp|*.webp";
+            if (openFileDialog1.ShowDialog()==DialogResult.OK)
+            {
+                string kepFajl = openFileDialog1.FileName;
+                pictureBox1.Image =Image.FromFile(kepFajl);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
